ca8a3a8a0f40611ae0c3d923d36dfe60bbb38e36
Artem Holod <artsiom.kholodok@gmail.com>
2021-10-18 11:52:06 +0200

    Initial Commit

---
+++ LikeTok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LikeTok
+//
+//  Created by Artem Holod on 18.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
